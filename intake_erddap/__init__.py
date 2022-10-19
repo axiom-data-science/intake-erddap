73f45444f2a5bfe88e3ac648b4fe24f53daec7a9
Luke Campbell <luke@axiomdatascience.com>
2022-10-19 15:20:40 -0400

    Basic project layout
    This commit adds some bits for package maintenance and CI integration.
    
    - pre-commit
    - conda environment for development
    - Versioning from git tags

--- intake_erddap/__init__.py
+++ intake_erddap/__init__.py
@@ -1,9 +1,14 @@
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
-
 import intake
 from .erddap import (ERDDAPSource, ERDDAPSourceAutoPartition,
                          ERDDAPSourceManualPartition)
 from .erddap_cat import ERDDAPCatalog
+
+
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution("model_catalogs").version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "unknown"
