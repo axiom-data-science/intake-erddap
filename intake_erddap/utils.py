d0ec53c709f4e1e23900c1443b1e74332d63bc4a
Kristen Thyng <kthyng@gmail.com>
2022-10-21 17:25:03 -0500

    precommit fixes

--- intake_erddap/utils.py
+++ intake_erddap/utils.py
@@ -3,9 +3,11 @@
 """Utility functions."""
 
 from typing import Optional
-from pandas import DataFrame
+
 import cf_pandas as cfp
 import pandas as pd
+
+from pandas import DataFrame
 
 
 def get_project_version() -> str:
@@ -18,44 +20,56 @@ def get_project_version() -> str:
     return __version__
 
 
-def return_category_options(server: str, category: Optional[str] = "standard_name",) -> DataFrame:
+def return_category_options(
+    server: str,
+    category: Optional[str] = "standard_name",
+) -> DataFrame:
     """Find category options for ERDDAP server.
-    
+
     Parameters
     ----------
     server : str
         ERDDAP server address, for example: "http://erddap.sensors.ioos.us/erddap"
     category : str, optional
-        ERDDAP category for filtering results. Default is "standard_name" but another good option is "variableName".
-    
+        ERDDAP category for filtering results. Default is "standard_name" but another good option is
+        "variableName".
+
     Returns
     -------
     DataFrame
-        Column "Category" contains all options for selected category on server. Column "URL" contains the link for search results for searching for a given category value.
+        Column "Category" contains all options for selected category on server. Column "URL" contains
+        the link for search results for searching for a given category value.
     """
 
-    df = pd.read_csv(f"{server}/categorize/{category}/index.csv?page=1&itemsPerPage=100000")
+    df = pd.read_csv(
+        f"{server}/categorize/{category}/index.csv?page=1&itemsPerPage=100000"
+    )
 
     return df
 
 
-def match_key_to_category(server: str, 
+def match_key_to_category(
+    server: str,
     key: str,
     category: Optional[str] = "standard_name",
-    criteria: Optional[dict] = None,) -> list:
+    criteria: Optional[dict] = None,
+) -> list:
     """Find category values for server and return match to key.
-    
+
     Parameters
     ----------
     server : str
         ERDDAP server address, for example: "http://erddap.sensors.ioos.us/erddap"
     key : str
-        The custom_criteria key to narrow the search, which will be matched to the category results using the custom_criteria that must be set up ahead of time with `cf-pandas`.
+        The custom_criteria key to narrow the search, which will be matched to the category results
+        using the custom_criteria that must be set up ahead of time with `cf-pandas`.
     category : str, optional
-        ERDDAP category for filtering results. Default is "standard_name" but another good option is "variableName".
+        ERDDAP category for filtering results. Default is "standard_name" but another good option
+        is "variableName".
     criteria : dict, optional
-        Criteria to use to map from variable to attributes describing the variable. If user has defined custom_criteria, this will be used by default.
-    
+        Criteria to use to map from variable to attributes describing the variable. If user has
+        defined custom_criteria, this will be used by default.
+
     Returns
     -------
     list
@@ -63,6 +77,8 @@ def match_key_to_category(server: str,
     """
 
     df = return_category_options(server, category)
-    matching_category_value = cfp.match_criteria_key(df['Category'].values, key, criteria=criteria)
+    matching_category_value = cfp.match_criteria_key(
+        df["Category"].values, key, criteria=criteria
+    )
 
     return matching_category_value
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 """Tests for generic and utility functions."""
 from unittest import mock
-from intake_erddap import utils
+
 import pandas as pd
+
+from intake_erddap import utils
 
 
 def test_get_project_version():
@@ -19,7 +21,7 @@ def test_category_and_key(mock_read_csv):
     mock_read_csv.return_value = df_mock
     server = "http://erddap.invalid/erddap"
     df = utils.return_category_options(server, "standard_name")
-    assert set(df['Category']) - set(df_mock["Category"]) == set()
+    assert set(df["Category"]) - set(df_mock["Category"]) == set()
 
     # Also test match_key_to_category with this setup
     criteria = {
@@ -27,5 +29,7 @@ def test_category_and_key(mock_read_csv):
             "standard_name": "wind_speed$",
         },
     }
-    match_to_key = utils.match_key_to_category(server, "wind_s", "standard_name", criteria)
+    match_to_key = utils.match_key_to_category(
+        server, "wind_s", "standard_name", criteria
+    )
     assert match_to_key == ["wind_speed"]
