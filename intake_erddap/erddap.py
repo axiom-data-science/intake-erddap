c7c3be3ba2c47258ecbded90d92d3a923d0e964d
Martin Durant <martin.durant@alumni.utoronto.ca>
2024-07-04 10:44:37 -0400

    Remove stuff

--- intake_erddap/erddap.py
+++ intake_erddap/erddap.py
@@ -12,23 +12,15 @@ import requests
 import xarray as xr
 
 from erddapy import ERDDAP
-from intake.source import base
 from intake.readers.readers import BaseReader
 from intake.readers.entry import ReaderDescription
 from intake.readers.datatypes import BaseData
-
-from .version import __version__
 
 
 log = getLogger("intake-erddap")
 
 
-if typing.TYPE_CHECKING:  # pragma: no cover
-    # numpy typing is only available after version 1.21
-    from numpy.typing import ArrayLike
-
-    
-class ERDDAPSource(BaseData):
+class ERDDAPSource(BaseReader):
     """
     ERDDAP Source (Base Class). This class represents the abstract base class
     for an intake data source object for ERDDAP. Clients should use either
@@ -60,51 +52,15 @@ class ERDDAPSource(BaseData):
     Caches entire dataframe in memory.
     """
 
-    name = "erddap"
-    version = __version__
-    container = "dataframe"
-    partition_access = True
+    output_instance = "xarray:Dataset"
 
-    def __init__(
-        self,
-        dataset_id: str,
-        protocol: str,
-        variables: List[str] = None,
-        constraints: dict = None,
-        metadata: dict = None,
-        erddap_client: Optional[Type[ERDDAP]] = None,
-        http_client: Optional[Type] = None,
-        open_kwargs: dict = None,
-    ):
-        variables = variables or []
-        constraints = constraints or {}
-        metadata = metadata or {}
-
-        self._init_args = {
-            "dataset_id": dataset_id,
-            "protocol": protocol,
-            "variables": variables,
-            "constraints": constraints,
-            "metadata": metadata,
-        }
-
-        self._dataset_id = dataset_id
-        self._protocol = protocol
-        self._variables = variables
-        self._constraints = constraints
-        self._erddap_client = erddap_client or ERDDAP
-        self._http = http_client or requests
-        self.open_kwargs = open_kwargs or {}
-
-        super(ERDDAPSource, self).__init__(metadata=metadata)
-
-    def get_client(self) -> ERDDAP:
+    def get_client(self, server, protocol, dataset_id, variables, constraints, client=ERDDAP, **_) -> ERDDAP:
         """Return an initialized ERDDAP Client."""
-        e = self._erddap_client(server=self._server)
-        e.protocol = self._protocol
-        e.dataset_id = self._dataset_id
-        e.variables = self._variables
-        e.constraints = self._constraints
+        e = client(server=server)
+        e.protocol = protocol
+        e.dataset_id = dataset_id
+        e.variables = variables
+        e.constraints = constraints
         return e
 
 
@@ -177,64 +133,14 @@ class TableDAPSource(ERDDAPSource):
         ...
     """
 
-    name = "tabledap"
-    version = __version__
-    container = "dataframe"
-    partition_access = True
-
-    def __init__(
-        self,
-        server: str,
-        mask_failed_qartod: bool = False,
-        dropna: bool = False,
-        cache_kwargs: Optional[dict] = None,
-        *args,
-        **kwargs,
-    ):
-        self._server = server
-        self._dataframe: Optional[pd.DataFrame] = None
-        self._dataset_metadata: Optional[dict] = None
-        self._mask_failed_qartod = mask_failed_qartod
-        self._dropna = dropna
-        self._cache_kwargs = cache_kwargs
-        kwargs.pop("protocol", None)
-        # https://github.com/python/mypy/issues/6799
-        super().__init__(*args, protocol="tabledap", **kwargs)  # type: ignore
-
-    def _get_schema(self) -> base.Schema:
-        if self._dataframe is None:
-            # TODO: could do partial read with chunksize to get likely schema from
-            # first few records, rather than loading the whole thing
-            self._load()
-            self._dataset_metadata = self._get_dataset_metadata()
-        # make type checker happy
-        assert self._dataframe is not None
-        return base.Schema(
-            datashape=None,
-            dtype=self._dataframe.dtypes,
-            shape=self._dataframe.shape,
-            npartitions=1,
-            extra_metadata=self._dataset_metadata,
-        )
-
-    def _get_partition(self) -> pd.DataFrame:
-        if self._dataframe is None:
-            self._load_metadata()
-        return self._dataframe
-
-    def read(self) -> pd.DataFrame:
-        """Return the dataframe from ERDDAP"""
-        # return self._get_partition()
-        if self._dataframe is None:
-            self._load()
-        return self._dataframe
-
-    def _close(self):
-        self._dataframe = None
-
-    def _load(self):
-        e = self.get_client()
-        if self._cache_kwargs is not None:
+    def _read(self, server, dataset_id, mask_failed_qartod=False, dropna=False, cache_kwargs=None,
+              constraints=None, **kw):
+        kw.pop("protocol", None)
+        protocol = kw.pop("protocol", "tabledap")
+        meta2 = self._get_dataset_metadata(server, dataset_id)
+        e = self.get_client(server, protocol, dataset_id, variables=meta2["variables"],
+                            constraints=constraints or {}, **kw)
+        if cache_kwargs is not None:
             if "response" in self.open_kwargs:
                 response = self.open_kwargs["response"]
                 self.open_kwargs.pop("response")
@@ -242,35 +148,34 @@ class TableDAPSource(ERDDAPSource):
             else:
                 url = e.get_download_url(response=response)
 
-            with fsspec.open(f"simplecache://::{url}", **self._cache_kwargs) as f:
-                self._dataframe: pd.DataFrame = pd.read_csv(f, **self.open_kwargs)
+            with fsspec.open(f"simplecache://::{url}", **(cache_kwargs or {})) as f:
+                dataframe: pd.DataFrame = pd.read_csv(f)
         else:
-            self._dataframe: pd.DataFrame = e.to_pandas(
-                requests_kwargs={"timeout": 60}, **self.open_kwargs
+            dataframe: pd.DataFrame = e.to_pandas(
+                requests_kwargs={"timeout": 60}
             )
-        if self._mask_failed_qartod:
-            self.run_mask_failed_qartod()
-        if self._dropna:
-            self.run_dropna()
+        if mask_failed_qartod:
+            dataframe = self.run_mask_failed_qartod(dataframe)
+        if dropna:
+            dataframe = self.run_dropna(dataframe)
+        return dataframe
 
-    @property
-    def data_cols(self):
+    @staticmethod
+    def data_cols(df):
         """Columns that are not axes, coordinates, nor qc_agg columns."""
 
         # find data columns which are what we'll use in the final step to drop nan's
         # don't include dimension/coordinates-type columns (dimcols) nor qc_agg columns (qccols)
-        dimcols = self._dataframe.cf.axes_cols + self._dataframe.cf.coordinates_cols
+        dimcols = df.cf.axes_cols + df.cf.coordinates_cols
         qccols = list(
-            self._dataframe.columns[self._dataframe.columns.str.contains("_qc_agg")]
+            df.columns[df.columns.str.contains("_qc_agg")]
         )
-
         datacols = [
-            col for col in self._dataframe.columns if col not in dimcols + qccols
+            col for col in df.columns if col not in dimcols + qccols
         ]
-
         return datacols
 
-    def run_mask_failed_qartod(self):
+    def run_mask_failed_qartod(self, df):
         """Nan data values for which corresponding qc_agg columns is not equal to 1 or 2.
 
         To get this to work you may need to specify the "qc_agg" columns to come along specifically
@@ -279,22 +184,23 @@ class TableDAPSource(ERDDAPSource):
 
         # if a data column has an associated qc column, use it to weed out bad data by
         # setting it to nan.
-        for datacol in self.data_cols:
+        for datacol in self.data_cols(df):
             qccol = f"{datacol}_qc_agg"
-            if qccol in self._dataframe.columns:
-                self._dataframe.loc[
+            if qccol in df.columns:
+                df.loc[
                     ~self._dataframe[qccol].isin([1, 2]), datacol
                 ] = pd.NA
-                self._dataframe.drop(columns=[qccol], inplace=True)
+                df.drop(columns=[qccol], inplace=True)
+        return df
 
-    def run_dropna(self):
+    def run_dropna(self, df):
         """Drop nan rows based on the data columns."""
-        self._dataframe = self._dataframe.dropna(subset=self.data_cols)
+        return df.dropna(subset=self.data_cols)
 
-    def _get_dataset_metadata(self) -> dict:
+    def _get_dataset_metadata(self, server, dataset_id) -> dict:
         """Fetch and return the metadata document for the dataset."""
-        url = f"{self._server}/info/{self._dataset_id}/index.json"
-        resp = self._http.get(url)
+        url = f"{server}/info/{dataset_id}/index.json"
+        resp = requests.get(url)
         resp.raise_for_status()
         metadata: dict = {"variables": {}}
         for rowtype, varname, attrname, dtype, value in resp.json()["table"]["rows"]:
@@ -396,11 +302,6 @@ class GridDAPSource(ERDDAPSource):
     method ``ds.load()`` on the Dataset object.
     """
 
-    name = "griddap"
-    version = __version__
-    container = "xarray"
-    partition_access = True
-
     def __init__(
         self,
         server: str,
@@ -411,88 +312,22 @@ class GridDAPSource(ERDDAPSource):
         **kwargs,
     ):
         self._server = server
-        self._ds: Optional[xr.Dataset] = None
         self._chunks = chunks
         self._constraints = constraints or {}
         self._xarray_kwargs = xarray_kwargs or {}
         # Initialized by the private getter _get_schema
-        self._schema: Optional[base.Schema] = None
         self.urlpath = f"{server}/griddap/{dataset_id}"
         # https://github.com/python/mypy/issues/6799
         kwargs.pop("protocol", None)
         super().__init__(dataset_id=dataset_id, protocol="griddap", **kwargs)  # type: ignore
 
-    def _get_schema(self) -> base.Schema:
-        self.urlpath = self._get_cache(self.urlpath)[0]
-
-        if self._ds is None:
-            # Sets self._ds
-            self._open_dataset()
-            # Make mypy happy
-            assert self._ds is not None
-            metadata = {
-                "dims": dict(self._ds.dims),
-                "data_vars": {
-                    k: list(self._ds[k].coords) for k in self._ds.data_vars.keys()
-                },
-                "coords": tuple(self._ds.coords.keys()),
-            }
-            metadata.update(self._ds.attrs)
-            metadata["variables"] = {}
-            for varname in self._ds.variables:
-                metadata["variables"][varname] = self._ds[varname].attrs
-            self._schema = base.Schema(
-                datashape=None,
-                dtype=None,
-                shape=None,
-                npartitions=None,
-                extra_metadata=metadata,
-            )
-
-        return self._schema
-
-    def _open_dataset(self):
-        self._ds = xr.open_dataset(
+    def _read(self):
+        ds = xr.open_dataset(
             self.urlpath, chunks=self._chunks, **self._xarray_kwargs
         )
         # _NCProperties is an internal property which xarray does not yet deal
         # with specially, so we remove it here to prevent it from causing
         # problems for clients.
-        if "_NCProperties" in self._ds.attrs:
-            del self._ds.attrs["_NCProperties"]
-
-    def read(self):
-        raise NotImplementedError(
-            "GridDAPSource.read is not implemented because ds.load() for grids from ERDDAP are "
-            "strongly discouraged. Use to_dask() instead."
-        )
-
-    def read_chunked(self) -> xr.Dataset:
-        """Return an xarray dataset (optionally chunked)."""
-        self._load_metadata()
-        return self._ds
-
-    def read_partition(self, i: Tuple[str, ...]) -> "ArrayLike":
-        """Fetch one chunk of the array for a variable."""
-        self._load_metadata()
-        if not isinstance(i, (tuple, list)):
-            raise TypeError("For Xarray sources, must specify partition as tuple")
-        if isinstance(i, list):
-            i = tuple(i)
-        # Make mypy happy
-        assert self._ds is not None
-        arr = self._ds[i[0]].data
-        idx = i[1:]
-        if isinstance(arr, np.ndarray):
-            return arr
-        # dask array
-        return arr.blocks[idx].compute()
-
-    def to_dask(self) -> xr.Dataset:
-        """Return an xarray dataset (optionally chunked)."""
-        return self.read_chunked()
-
-    def close(self):
-        """Close open descriptors."""
-        self._ds = None
-        self._schema = None
+        if "_NCProperties" in ds.attrs:
+            del ds.attrs["_NCProperties"]
+        return ds
